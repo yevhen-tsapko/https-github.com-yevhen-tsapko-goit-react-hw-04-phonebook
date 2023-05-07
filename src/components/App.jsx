c9ec592df2c7ee55654f64c32db78b62b03d1150
Євген Цапко <111687103+yevhen-tsapko@users.noreply.github.com>
2023-05-07 15:32:58 +0300

    delete unused code in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,7 @@ import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 
 export const App = () => {
-  // state = {
-  //   contacts: [
-  //     // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
+ 
   const [contacts, setContacts] = useState(() =>
     JSON.parse(localStorage.getItem('contacts') ?? [])
   );
@@ -41,16 +33,7 @@ export const App = () => {
   const removeContact = id => {
     setContacts(contacts.filter(contact => contact.id !== id));
   };
-  // const componentDidMount() {
-  //   const savedContacts = JSON.parse(localStorage.getItem('contacts'));
-  //   setContacts( savedContacts ? savedContacts : [] );
-  // }
-  // const componentDidUpdate(prevState) {
-  //   if (prevState.contacts !== this.state.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(contacts));
-  //     // console.log(this.state);
-  //   }
-  // }
+ 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
